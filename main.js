81314ad3159ce4a4be20f362f04a73b0357f710b
wasp <saliou.armony@gmail.com>
2018-12-22 17:55:34 +0100

    Mode psychédélique crée sur main.js (vide)

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 window.confirm("Wanna get laid ?");
-if (confirm("Press a button!")) {
+if (confirm("Karma bitch!")) {
   window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 } else {
   txt = "Smart move!";
 }
+var color = ["#FF00FF"];
+var i = 0;
+document.querySelector("button").addEventListner("click", function(){
+	i = i < color.length ? ++i : 0;
+document.querySelector("body").style.background = color[i]
+})
