cb4658866bf654648f89c61716caaacea380e134
wasp <saliou.armony@gmail.com>
2018-12-25 19:34:17 +0100

    button restauré sur main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 window.confirm("Wanna get laid ?");
-<<<<<<< HEAD
  if (confirm("Karma bitch!")) {
   window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
    } else {
    txt = "Smart move!";
-=======
-if (confirm("Press a button!")) {
-  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-} else {
-  txt = "Smart move!";
->>>>>>> parent of 81314ad... Mode psychédélique crée sur main.js (vide)
 }
+var color = ["#FF00FF"];
+var i = 0;
+document.querySelector("button").addEventListener("click", function(){
+	console.log(color());
+	i = i < color.length ? ++i : 0;
+	document.querySelector("body").style.background = color[i]});
