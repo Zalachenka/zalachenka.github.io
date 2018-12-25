bcfcb2931076cbd73f2bd28aca647bca1f6b3259
wasp <saliou.armony@gmail.com>
2018-12-25 19:06:51 +0100

    main.js modif'

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.confirm("Wanna get laid ?");
-if (confirm("Karma bitch!")) {
-  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-} else {
-  txt = "Smart move!";
+ if (confirm("Karma bitch!")) {
+  window.location.replace("youtube.com/watch?v=dQw4w9WgXcQ");
+   } else {
+   txt = "Smart move!";
 }
-var color = ["#FF00FF"];
-var i = 0;
+ var color = ["#FF00FF"];
+ var i = 0;
 document.querySelector("button").addEventListener("click", function (){
-
-	i = i < color.length ? ++i : 0;
-	document.querySelector("body").style.background = color[i]});
+console.log(color());
+    i = i < color.length ? ++i : 0;
+   document.querySelector("body").style.background = color[i]});
