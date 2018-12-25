6ca104dc0fd780c03f79bad1c070bd5a85e4b85d
wasp <saliou.armony@gmail.com>
2018-12-25 19:25:52 +0100

    modified moncv.html

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 window.confirm("Wanna get laid ?");
+<<<<<<< HEAD
  if (confirm("Karma bitch!")) {
   window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
    } else {
    txt = "Smart move!";
+=======
+if (confirm("Press a button!")) {
+  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+} else {
+  txt = "Smart move!";
+>>>>>>> parent of 81314ad... Mode psychédélique crée sur main.js (vide)
 }
