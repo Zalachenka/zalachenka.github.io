c3ea73760660f363547ffea41d951ac37b4b83fe
wasp <saliou.armony@gmail.com>
2018-12-26 12:55:55 +0100

    essai button non concluant

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ window.confirm("Wanna get laid ?");
   window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
    } else {
    txt = "Smart move!";
-}
+};
+document.getElementById("Psych").click() = chBackgroundColor ;
+
+    function changeColor() {
+        document.body.style.BackgroundColor = "magenta";
+        return false;
+    }
+var el = document.getElementById("Psych");
+if (el.addEventListener)
+    el.addEventListener("click", doFunction, false);
+else if (el.attachEvent)
+    el.attachEvent('onclick', doFunction);
+
