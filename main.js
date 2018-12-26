ac3147252a2c0a8cb16bd4f60f0a5f200d72f219
wasp <saliou.armony@gmail.com>
2018-12-26 16:03:28 +0100

    Heading color green on Mode Psyché

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ window.confirm("Wanna get laid ?");
    document.body.style.background = color;
 };
 function changeColor(color) {
-    document.querySelectorAll('.forma').forEach(function(ele) {
-        ele.style.color = color});
+    document.querySelectorAll('.forma, .descrip').forEach(function(ele) {
+        ele.style.color = color });
+};
+function changeHeadingColor(color){
+	var matches = document.querySelectorAll("h2");
+document.querySelectorAll('h2').forEach(function(ele) {
+		ele.style.color = color });
 };
