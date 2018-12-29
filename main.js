172ce6940c41e6877ebf14be5e88ebc432be695d
wasp <saliou.armony@gmail.com>
2018-12-29 17:36:57 +0100

    tentatives papyrus échec

--- main.js
+++ main.js
@@ -12,13 +12,10 @@ function changeColor(color) {
         ele.style.color = color });
 };
 function changeHeadingColor(color){
-	var matches = document.querySelectorAll("h2");
 document.querySelectorAll('h2').forEach(function(ele) {
 		ele.style.color = color });
 };
 function changeFontFamily(fontFamily){
-	var matches = document.querySelectorAll("p");
-	document.querySelectorAll('p').forEach(function(ele) {
-		DomElement.style.fontFamily = papyrus });
+	document.querySelectorAll('.forma, .descrip').forEach(function(ele) {
+		ele.style.fontFamily = fontFamily });
 	};
-	
