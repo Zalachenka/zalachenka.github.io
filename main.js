9ae47cecade9b17218a2c64cebde4ba4818f5879
wasp <saliou.armony@gmail.com>
2019-03-14 19:51:18 +0100

    papyrus success

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.confirm("Wanna get laid ?");
- if (confirm("Karma bitch!")) {
-  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+if (confirm("Karma bitch!")) {
+ window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
    } else {
    txt = "Smart move!";
 };
